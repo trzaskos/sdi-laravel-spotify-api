6261acd50db8537891f55a8452e212c6960c16e4
Maryele Trzaskos Gruber <maryele.trzaskos@gmail.com>
2025-05-20 17:58:10 +0200

    feat: protected music routes using auth:sanctum middleware

--- routes/api.php
+++ routes/api.php
@@ -5,17 +5,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
 
 Route::prefix('auth')->group(function () {
-    // Register new user
     Route::post('/register', [AuthController::class, 'register']);
-
-    // Login and receive token
     Route::post('/login', [AuthController::class, 'login']);
-
-    // Logout (protected)
     Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 });
 
-Route::prefix('music')->group(function () {
+Route::middleware('auth:sanctum')->prefix('music')->group(function () {
     Route::get('/artists', [MusicController::class, 'searchArtists']);
     Route::get('/playlists/{id}', [MusicController::class, 'getPlaylist']);
 });

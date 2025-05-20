1d3c8dd58bfdaa4694c5f866e673c2a0d6c721c0
Maryele Trzaskos Gruber <maryele.trzaskos@gmail.com>
2025-05-20 14:25:08 +0200

    configure api routes with Sanctum middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,9 +6,18 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
+        api: [
+            'prefix' => 'api',
+            'middleware' => [
+                \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+                'throttle:api',
+                \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            ],
+            'using' => __DIR__ . '/../routes/api.php',
+        ],
     )
     ->withMiddleware(function (Middleware $middleware) {
         //

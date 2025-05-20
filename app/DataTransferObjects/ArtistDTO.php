ffe3da242cd3db5299b69021eadc6a6799b906b9
Maryele Trzaskos Gruber <maryele.trzaskos@gmail.com>
2025-05-20 17:12:18 +0200

    feat: add ArtistDTO and PlaylistDTO using MusicSource enum for multi-platform support

---
+++ app/DataTransferObjects/ArtistDTO.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataTransferObjects;
+
+use App\Enums\MusicSource;
+
+class ArtistDTO
+{
+    public function __construct(
+        public readonly string $id,
+        public readonly string $name,
+        public readonly int|null $popularity,
+        public readonly array $genres,
+        public readonly string|null $image,
+        public readonly MusicSource $source,
+    ) {}
+
+    public static function fromSpotify(array $data): self
+    {
+        return new self(
+            id: $data['id'],
+            name: $data['name'],
+            popularity: $data['popularity'] ?? null,
+            genres: $data['genres'] ?? [],
+            image: $data['images'][0]['url'] ?? null,
+            source: MusicSource::SPOTIFY,
+        );
+    }
+}
---
+++ app/Enums/MusicSource.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums;
+
+enum MusicSource: string
+{
+    case SPOTIFY = 'spotify';
+    case DEEZER = 'deezer';
+    case YOUTUBE = 'youtube';
+}
